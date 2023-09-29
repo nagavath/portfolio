8142237f8df58da428bf38b5cb8da53700becccb
nagavath <109595753+nagavath@users.noreply.github.com>
2023-09-30 04:12:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var Typed= new Typed (".text", {
+    strings:["Web Developer", "Frontend Developer", "Backend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1500,
+    loop:true
+}
+);
